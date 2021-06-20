6b8f83b29098d297d0e94d1db93e542996575bb5
Ben Lee <ben@simplelife.com>
2021-06-20 15:08:02 -0700

    add more useful messages

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ cd ~/
 git clone https://github.com/bitclout/core.git
 cd core
 go build
+echo "Running bitcout in background..."
 ./core run > /dev/null 2>&1 &
 cd ~/balajis-script
 go mod vendor
@@ -16,7 +17,10 @@ mkdir /root/acopy
 pip3 install -r requirements.txt
 echo "Sleeping..."
 sleep 86400
+echo "Copying badgerdb..."
 cp -r /root/.config/bitclout/bitclout/MAINNET/badgerdb /root/acopy
 rm /root/acopy/badgerdb/*.mem
+echo "Extracting graph edges..."
 ./balajis /root/acopy/badgerdb > edges.txt
+echo "Running visualizer..."
 python3 visualize.py
