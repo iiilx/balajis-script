02519982f75231b4998f57e83435e50167217bfe
Ben Lee <ben@simplelife.com>
2021-06-20 00:38:32 -0700

    sleep after installing other requirements and compiling the script to extract follows

--- run.sh
+++ run.sh
@@ -7,16 +7,16 @@ git clone https://github.com/bitclout/core.git
 cd core
 go build
 ./core run > /dev/null 2>&1 &
-sleep 86400
-
 cd ..
 git clone https://github.com/andrewarrow/balajis-script.git
 cd balajis-script
 go mod vendor
 go build
 mkdir /root/acopy
-copy -r /root/.config/bitclout/bitclout/MAINNET/badgerdb /root/acopy
+pip3 install -r requirements.txt
+echo "Sleeping..."
+sleep 86400
+cp -r /root/.config/bitclout/bitclout/MAINNET/badgerdb /root/acopy
 rm /root/acopy/badgerdb/*.mem
 ./balajis /root/acopy/badgerdb > edges.txt
-pip3 install -r requirements.txt
 python3 visualize.py
